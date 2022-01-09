eb164812fb5fdbf799b97f23efc6c060d0b55b4c
Phiwe <wycliffwhite7@gmail.com>
2022-01-08 19:12:43 -0500

    added js file and iniated variables

---
+++ script.js
@@ -0,0 +1,25 @@
+
+let bill = document.getElementById('bill').value, fivePer = document.getElementById('btn1'),
+tenPer = document.getElementById('btn2'),
+fifteenPer = document.getElementById('btn3'),
+twentyFivePer = document.getElementById('btn4'),
+fiftyPer = document.getElementById('btn5'),
+customPer = document.getElementById('btn6').value,
+numberOfPeople = document.getElementById('no').value,
+resetBtn = document.getElementById('reset'),
+tipAmountVal = document.getElementById('tip-person'),
+totalPerPersonVal = document.getElementById('total-person'),
+
+// tip amount per person
+tipAmount = 0,
+
+// bill per person before tip
+billAmount = 0,
+
+// total/person (tip/person + bill/prsn)
+totalPerPerson = 0;
+
+function percentageTipPerson(percentageTip) {
+    
+}
+
